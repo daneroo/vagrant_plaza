cc61759afda591471aa1386a3771457c79e26443
Daniel Lauzon <daniel.lauzon@gmail.com>
2011-04-24 11:32:52 -0400

    added required ruby1.8-dev package required for mysql gem

--- gitorious/cookbooks/vagrant_main/recipes/default.rb
+++ gitorious/cookbooks/vagrant_main/recipes/default.rb
@@ -2,16 +2,16 @@
 
 #this runs apt-get update update
 require_recipe "apt"
+# required otherwise the mysql gem will not install
+package 'ruby1.8-dev'
 
 Chef::Log.info("DD:mypswd #{node[:mysql][:server_root_password]}")
 Chef::Log.info("DD:gitpswd #{node[:gitorious][:db][:password]}")
 #Chef::Log.info("DD-:rvmroot #{node[:rvm][:root_path]}")
 
-# required otherwise the mysql gem will not install
-package 'ruby1.8-dev'
-
 require_recipe "vagrant_extras"
 require_recipe "mysql::server"
+require_recipe "rvm"
 require_recipe "rvm_passenger"
 require_recipe "gitorious"
 
